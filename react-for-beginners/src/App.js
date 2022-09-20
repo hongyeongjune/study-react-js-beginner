e47d84be7e0aac80b14dea1ca3936c1a9e6dd8e8
hongyeongjune <hongyeongjune@naver.com>
2022-09-20 22:32:26 +0900

    React Router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,8 +1,19 @@
 import Home from './route/Home';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Detail from './route/Details';
 
 function App() {
     return (
-        <Home />
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
